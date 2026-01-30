6716e46f91a9583bf00858a169e487c523f14d49
Ambarish Manna <amanna1041@gmail.com>
2026-01-30 14:08:11 +0530

    chore: add missing modules from kmp-template

--- cmp-shared/src/nativeMain/kotlin/org/mifos/shared/ViewController.kt
+++ cmp-shared/src/nativeMain/kotlin/org/mifos/shared/ViewController.kt
@@ -7,11 +7,16 @@
  *
  * See https://github.com/openMF/mobile-mobile/blob/master/LICENSE.md
  */
+@file:Suppress("UnusedPrivateMember")
+
 package org.mifos.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
 import cmp.shared.SharedApp
 import cmp.shared.utils.initKoin
+import platform.Foundation.NSUserDefaults
+import platform.UIKit.UIApplication
+import platform.UIKit.UIUserInterfaceStyle
 
 fun viewController() = ComposeUIViewController(
     configure = {
@@ -19,8 +24,24 @@ fun viewController() = ComposeUIViewController(
     },
 ) {
     SharedApp(
-        handleThemeMode = {},
-        handleAppLocale = {},
+        handleThemeMode = { osValue ->
+            val style = when (osValue) {
+                1 -> UIUserInterfaceStyle.UIUserInterfaceStyleLight
+                2 -> UIUserInterfaceStyle.UIUserInterfaceStyleDark
+                else -> UIUserInterfaceStyle.UIUserInterfaceStyleUnspecified
+            }
+            UIApplication.sharedApplication.keyWindow?.overrideUserInterfaceStyle = style
+        },
+        handleAppLocale = { languageTag ->
+            if (languageTag != null) {
+                // Set specific language
+                NSUserDefaults.standardUserDefaults.setObject(listOf(languageTag), forKey = "AppleLanguages")
+            } else {
+                // System Default: remove app-specific language setting
+                NSUserDefaults.standardUserDefaults.removeObjectForKey("AppleLanguages")
+            }
+            NSUserDefaults.standardUserDefaults.synchronize()
+        },
         onSplashScreenRemoved = {},
     )
 }
